a74cb680ff7d1dcdbfc5a3feed81fbe4b69ccad8
Brad Van Vugt <bvanvugt@gmail.com>
2015-01-25 17:55:01 -0800

    Basic things.

---
+++ server.py
@@ -0,0 +1,17 @@
+import bottle
+
+
+@bottle.get('/')
+def index():
+    return bottle.static_file('html/index.html', root='static')
+
+
+@bottle.get('/static/<filepath:path>')
+def server_static(filepath):
+    return bottle.static_file(filepath, root='static')
+
+
+if __name__ == '__main__':
+    bottle.run(host='localhost', port=8080)
+else:
+    app = bottle.default_app()

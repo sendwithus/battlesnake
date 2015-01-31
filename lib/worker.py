a079e7522eb88fe900794539e7eda162b612725a
Gregory Schier <gschier1990@gmail.com>
2015-01-31 14:24:34 -0800

    Basic worker skeleton

--- lib/worker.py
+++ lib/worker.py
@@ -1,11 +1,30 @@
+import time
+
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 
 db = client['battlesnake']
 
-games = db['games']
-game_states = db['game_states']
+
+def run_game(game):
+    print 'Running game %s' % game
 
 
-class (object):
+def check_for_games():
+    games = db.games.find({'status': 'waiting'}).sort('-created').limit(1)
+    if games.count():
+        run_game(games[0])
+        return True
+    else:
+        print 'No Games'
+        return False
+
+
+def main():
+    while True:
+        check_for_games()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()

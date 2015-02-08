c8b7cf9600e7d8fa728b18b1664e20925be675c7
Gregory Schier <gschier1990@gmail.com>
2015-02-07 16:35:01 -0800

    Drawing board almost works

--- static/js/Board.js
+++ static/js/Board.js
@@ -32,56 +32,51 @@ Board.prototype.resize = function() {
   this.canvas.width  = this.SQUARE_SIZE * this.dimensions[0];
   this.canvas.height = this.SQUARE_SIZE * this.dimensions[1];
 
-  this.update(this.gameState);
+  // this.update(this.gameState);
 };
 
-Board.prototype.init = function (gameState, updateCallback) {
-  this.updateCallback = updateCallback;
-  this.gameState = gameState;
-  this.dimensions = this.getBoardDimensions(this.gameState.board);
-  this.update(gameState);
+Board.prototype.init = function (width, height) {
+  this.gameState = null;
+  this.dimensions = [ width, height ];
 
-  // Needs to be called twice because the first one might trigger scrollbars
-  // to be shown, so the window size changes.
-  this.resize();
   this.resize();
 
   window.onresize = this.resize.bind(this);
 };
 
-Board.prototype.getGameId = function () {
-  return this.gameState.id;
-};
+// Board.prototype.getGameId = function () {
+//   return this.gameState.id;
+// };
+//
+// Board.prototype.isGameOver = function () {
+//   return this.gameState.game_over;
+// };
 
-Board.prototype.isGameOver = function () {
-  return this.gameState.game_over;
-};
-
-Board.prototype.kick = function () {
-  this.isStarted = true;
-
-  // DON'T KICK IF EXPECTING LOCAL INPUT
-  if (this.testPlayer) { return; }
-
-  this.tick();
-};
-
-Board.prototype.tick = function () {
-  var that = this;
-
-  this.yell(false, function () {
-    var soonestNextTick = that.lastTick + snakewithus.MOVE_DELTA;
-    var delta = soonestNextTick - Date.now();
-
-    // Keep above 0
-    delta = Math.max(0, delta);
-
-    setTimeout(function () {
-      that.tick.call(that);
-      that.lastTick = Date.now();
-    }, delta);
-  });
-};
+// Board.prototype.kick = function () {
+//   this.isStarted = true;
+//
+//   // DON'T KICK IF EXPECTING LOCAL INPUT
+//   if (this.testPlayer) { return; }
+//
+//   this.tick();
+// };
+//
+// Board.prototype.tick = function () {
+//   var that = this;
+//
+//   this.yell(false, function () {
+//     var soonestNextTick = that.lastTick + snakewithus.MOVE_DELTA;
+//     var delta = soonestNextTick - Date.now();
+//
+//     // Keep above 0
+//     delta = Math.max(0, delta);
+//
+//     setTimeout(function () {
+//       that.tick.call(that);
+//       that.lastTick = Date.now();
+//     }, delta);
+//   });
+// };
 
 Board.prototype.beginAnimation = function () {
   var that = this;
@@ -93,87 +88,88 @@ Board.prototype.beginAnimation = function () {
 
 Board.prototype.animate = function () {
   var boardData = this.gameState.board;
-  for (var y=0; y<boardData.length; y++) {
+  for (var y = 0; y < boardData.length; y++) {
     var row = boardData[y];
-    for (var x=0; x<row.length; x++) {
+    for (var x = 0; x < row.length; x++) {
       var square = row[x];
       var color = generateColor();
       color = makeNonGray(color, 150);
-      var colorStr = 'rgb('+color.join(',')+')';
+      var colorStr = 'rgb(' + color.join(',') + ')';
       this.fillSquare(x, y, colorStr);
     }
   }
 };
+//
+// Board.prototype.yell = function (localPlayerMove, callback) {
+//   if (!this.isStarted || this.isGameOver()) { return; }
+//   var data = {
+//     game_id: this.gameState.id
+//   };
+//
+//   if (localPlayerMove) {
+//     data.local_player_move = {
+//       player_id: this.testPlayer.id,
+//       data: {
+//         move: localPlayerMove
+//       }
+//     };
+//   }
+//
+//   var that = this;
+//
+//   $.ajax({
+//     type: 'PUT',
+//     contentType: 'application/json',
+//     dataType: 'json',
+//     url: '/game.tick/'+this.getGameId(),
+//     data: JSON.stringify(data)
+//   }).done(function(gameState) {
+//     that.update(gameState);
+//     if (typeof callback === 'function') {
+//       callback();
+//     }
+//   });
+// };
 
-Board.prototype.yell = function (localPlayerMove, callback) {
-  if (!this.isStarted || this.isGameOver()) { return; }
-  var data = {
-    game_id: this.gameState.id
-  };
-
-  if (localPlayerMove) {
-    data.local_player_move = {
-      player_id: this.testPlayer.id,
-      data: {
-        move: localPlayerMove
-      }
-    };
-  }
-
-  var that = this;
-
-  $.ajax({
-    type: 'PUT',
-    contentType: 'application/json',
-    dataType: 'json',
-    url: '/game.tick/'+this.getGameId(),
-    data: JSON.stringify(data)
-  }).done(function(gameState) {
-    that.update(gameState);
-    if (typeof callback === 'function') {
-      callback();
-    }
-  });
-};
-
-Board.prototype.enableTestMode = function (testPlayer) {
-  this.testPlayer = testPlayer || false;
-
-  var that = this;
-  $('body').on('keydown', function (e) {
-    that.localMove(e);
-  });
-};
-
-Board.prototype.localMove = function (e) {
-  if (!this.testPlayer) { return; }
-
-  key = e.keyCode;
-  if (key === snakewithus.KEYS.UP) {
-    e.preventDefault();
-    this.yell(snakewithus.DIRECTIONS.NORTH);
-  } else if (key === snakewithus.KEYS.DOWN) {
-    e.preventDefault();
-    this.yell(snakewithus.DIRECTIONS.SOUTH);
-  } else if (key === snakewithus.KEYS.LEFT) {
-    e.preventDefault();
-    this.yell(snakewithus.DIRECTIONS.WEST);
-  } else if (key === snakewithus.KEYS.RIGHT) {
-    e.preventDefault();
-    this.yell(snakewithus.DIRECTIONS.EAST);
-  }
-};
-
+// Board.prototype.enableTestMode = function (testPlayer) {
+//   this.testPlayer = testPlayer || false;
+//
+//   var that = this;
+//   $('body').on('keydown', function (e) {
+//     that.localMove(e);
+//   });
+// };
+//
+// Board.prototype.localMove = function (e) {
+//   if (!this.testPlayer) { return; }
+//
+//   key = e.keyCode;
+//   if (key === snakewithus.KEYS.UP) {
+//     e.preventDefault();
+//     this.yell(snakewithus.DIRECTIONS.NORTH);
+//   } else if (key === snakewithus.KEYS.DOWN) {
+//     e.preventDefault();
+//     this.yell(snakewithus.DIRECTIONS.SOUTH);
+//   } else if (key === snakewithus.KEYS.LEFT) {
+//     e.preventDefault();
+//     this.yell(snakewithus.DIRECTIONS.WEST);
+//   } else if (key === snakewithus.KEYS.RIGHT) {
+//     e.preventDefault();
+//     this.yell(snakewithus.DIRECTIONS.EAST);
+//   }
+// };
+//
 Board.prototype.update = function (gameState) {
   this.gameState = gameState;
 
   this.canvas.width = this.canvas.width;
+  // console.log('UPDATE', gameState);
 
   var boardData = gameState.board;
 
-  for (var y=0; y<boardData.length; y++) {
+  for (var y = 0; y < boardData.length; y++) {
     var row = boardData[y];
-    for (var x=0; x<row.length; x++) {
+    for (var x = 0; x < row.length; x++) {
       var square = row[x];
       this.drawSquare(x, y, square);
     }
@@ -183,10 +179,10 @@ Board.prototype.update = function (gameState) {
     this.updateCallback(gameState);
   }
 
-  if (this.isGameOver()) {
-    clearInterval(this.loop);
-    this.beginAnimation();
-  }
+  // if (this.isGameOver()) {
+  //   clearInterval(this.loop);
+  //   this.beginAnimation();
+  // }
 };
 
 Board.prototype.drawSquare = function (x, y, square) {
@@ -197,41 +193,49 @@ Board.prototype.drawSquare = function (x, y, square) {
   }
 
   var snake;
-  var square_obj = square[0];
+  var head;
 
   this.fillSquare(x, y, snakewithus.COLORS.EMPTY);
-  if (!square_obj) {
+
+  if (square.state === snakewithus.SQUARE_TYPES.EMPTY) {
     // Nothing
-  } else if (square_obj.type === snakewithus.SQUARE_TYPES.SNAKE) {
-    snake = this.getSnake(square_obj.id);
+  }
+
+  // Draw body
+  else if (square.state === snakewithus.SQUARE_TYPES.SNAKE) {
+    snake = this.getSnake(square.snake_id);
     this.fillSquare(x, y, snake.getColor());
-  } else if (square_obj.type === snakewithus.SQUARE_TYPES.SNAKE_HEAD) {
-    snake = this.getSnake(square_obj.id);
+  }
+
+  // Draw head
+  else if (square.state === snakewithus.SQUARE_TYPES.SNAKE_HEAD) {
+    snake = this.getSnake(square.snake_id);
     head = snake.getHeadImage();
     if (head) { this.drawImage(x, y, head); }
     else { this.fillSquare(x, y, snake.getHeadColor()); }
-  } else if (square_obj.type === snakewithus.SQUARE_TYPES.FOOD) {
+  }
+
+  // Draw food
+  else if (square.state === snakewithus.SQUARE_TYPES.FOOD) {
     this.fillCircle(x, y, snakewithus.COLORS.FOOD);
-  } else {
-    console.error('INVALID SQUARE TYPE', square_obj.type);
+  }
+
+  else {
+    console.error('INVALID SQUARE TYPE', square.state);
   }
 };
 
 Board.prototype.getSnake = function (id) {
-  var snake_config = null;
+  var snake_data = null;
   for (var i = 0; i < this.gameState.snakes.length; i++) {
-    var config = this.gameState.snakes[i];
-    if (config.id === id) {
-      snake_config = config;
+    var s = this.gameState.snakes[i];
+    if (s.snake_id === id) {
+      snake_data = s;
+      break;
     }
   }
-  var snake = this.snakeCache[snake_config.id];
 
-  if (!snake) {
-    snake = this.snakeCache[snake_config.id] = new Snake(snake_config);
-  }
-
-  return snake;
+  return new Snake(snake_data);
 };
 
 Board.prototype.getBoardDimensions = function (board) {
@@ -273,7 +277,7 @@ Board.prototype.fillCircle = function (x, y, color) {
   var halfSquare = Math.round(this.SQUARE_SIZE / 2);
   var xCenter = x * this.SQUARE_SIZE + halfSquare;
   var yCenter = y * this.SQUARE_SIZE + halfSquare;
-  var radius = this.SQUARE_SIZE/2 * snakewithus.FOOD_SIZE;
+  var radius = this.SQUARE_SIZE / 2 * snakewithus.FOOD_SIZE;
 
   this.ctx.beginPath();
   this.ctx.arc(xCenter, yCenter, radius, 0, 2 * Math.PI);
@@ -283,28 +287,28 @@ Board.prototype.fillCircle = function (x, y, color) {
 
 Board.prototype.shouldBeFood = function () {
   var luckyNumber = (1 / snakewithus.FOOD_PROBABILITY);
-  var couldBeZero = Math.floor(Math.random()*luckyNumber);
+  var couldBeZero = Math.floor(Math.random() * luckyNumber);
 
   return !couldBeZero;
 };
 
-Board.prototype.genBoard = function (width, height) {
-  var board = [ ];
-
-  for (var y=0; y<height; y++) {
-    var row = [ ];
-    for (var x=0; x<width; x++) {
-      var square = [ ];
-
-      if (this.shouldBeFood()) {
-        square.push({
-          id: 'ntsir',
-          type: snakewithus.SQUARE_TYPES.FOOD
-        });
-      }
-      row.push(square);
-    }
-    board.push(row);
-  }
-  return board;
-};
+// Board.prototype.genBoard = function (width, height) {
+//   var board = [ ];
+//
+//   for (var y = 0; y < height; y++) {
+//     var row = [ ];
+//     for (var x = 0; x < width; x++) {
+//       var square = [ ];
+//
+//       if (this.shouldBeFood()) {
+//         square.push({
+//           id: 'ntsir',
+//           type: snakewithus.SQUARE_TYPES.FOOD
+//         });
+//       }
+//       row.push(square);
+//     }
+//     board.push(row);
+//   }
+//   return board;
+// };

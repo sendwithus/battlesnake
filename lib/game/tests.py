d328832229054c490279bb58549549783d2f3c10
Curtis Smith <curtis.smith8@gmail.com>
2015-02-20 15:25:42 -0800

    add test

--- lib/game/tests.py
+++ lib/game/tests.py
@@ -72,6 +72,27 @@ def test_head_to_head():
     assert(g.to_string().strip() == end_state.strip())
 
 
+def test_head_to_head_with_food():
+    game = Game(width=10, height=10)
+    g = Engine.create_game_state(game.id, game.width, game.height)
+    Engine.add_snakes_to_board(g, [SNAKE_1, SNAKE_2])
+    Engine.add_food_to_board(g, [3, 1])
+    g.sanity_check()
+
+    with open('lib/game/data/head_to_head_with_food.in') as f:
+        start_state = f.read().strip()
+
+    assert(g.to_string().strip() == start_state)
+
+    g = Engine.resolve_moves(g, [MOVE_RIGHT_1, MOVE_LEFT_2])
+    g.sanity_check()
+
+    with open('lib/game/data/head_to_head_with_food.out') as f:
+        end_state = f.read().strip()
+
+    assert(g.to_string().strip() == end_state.strip())
+
+
 def test_head_to_body():
     game = Game(width=10, height=10)
     g = Engine.create_game_state(game.id, game.width, game.height)

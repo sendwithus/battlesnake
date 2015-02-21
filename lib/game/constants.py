0b3fdba1d2b5972b346b822ac86839051bea1fe6
Curtis Smith <curtis.smith8@gmail.com>
2015-02-20 22:01:14 -0800

    trying less food

--- lib/game/constants.py
+++ lib/game/constants.py
@@ -1,5 +1,5 @@
 MAX_FOOD_ON_BOARD = 25  # Max food on board
 EAT_RATIO = 0.50  # Ammount of snake length absorbed
-TURNS_PER_FOOD = 3  # Turns between food spawning
+TURNS_PER_FOOD = 7  # Turns between food spawning
 HUNGER_THRESHOLD = 100  # Turns of inactivity before snake starvation
 SNAKE_STARTING_LENGTH = 3  # Snake starting size

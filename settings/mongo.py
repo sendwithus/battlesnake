84e0ddd86713e22488a6727876a230594a49459c
Rob O'Dwyer <odwyerrob@gmail.com>
2015-12-07 20:48:02 -0800

    use database in mongo url

--- settings/mongo.py
+++ settings/mongo.py
@@ -1,5 +1,5 @@
 from os import environ
 
-MONGODB_URL = environ.get('MONGODB_HOST', 'mongodb://localhost:27017/battlesnake')
 MONGODB_DATABASE = environ.get('MONGODB_DATABASE', 'battlesnake')
+MONGODB_URL = environ.get('MONGODB_URL', 'mongodb://localhost:27017/{}'.format(MONGODB_DATABASE))
 MONGODB_REPLICA_SET = environ.get('MONGODB_REPLICA_SET', None)

e161b6b1cf634519557813e99b5f3892808bdeec
Sean Lynch <seanlynch@gmail.com>
2015-12-18 14:43:32 -0800

    Proposal for addressing @robbles comments

--- lib/routes/teams.py
+++ lib/routes/teams.py
@@ -3,12 +3,42 @@ from flask.ext.login import login_required
 
 from lib.server import _json_response, app
 
-@app.route('/api/current_team')
+@app.route('/api/team')
 @login_required
 def team_info():
     return _json_response(data={
         'teamname': g.team.teamname
     })
+
+
+@app.route('/api/team/members/<email>', methods=['PUT'])
+@login_required
+def team_member_create(email):
+    """
+    Add a new member to an existing team.
+    Sample request:
+    PUT /api/teams/TEAM1/members/user2@domain.com
+    Sample response:
+    HTTP 201
+    {
+      "data": {
+        "_id": "TEAM1",
+        "member_emails": ["user@domain.com", "user2@domain.com"],
+        "snake_url": "http://localhost:6000/"
+      },
+      "message": "Member added"
+    }
+    """
+
+    if email not in g.team.member_emails:
+        g.team.member_emails.append(email)
+
+        g.team.save()
+
+        return _json_response(g.team.member_emails, msg='Member added', status=201)
+
+    return _json_response(g.team.member_emails, msg='Member already exists', status=200)
+
 
 
 # TODO: authorization on routes that modify teams
@@ -92,34 +122,3 @@ def team_details(teamname):
     if not team:
         return _json_response(msg='Team not found', status=404)
     return _json_response(team.to_dict())
-
-
-@app.route('/api/teams/<teamname>/members/<email>', methods=['PUT'])
-def team_member_create(teamname, email):
-    """
-    Add a new member to an existing team.
-    Sample request:
-    PUT /api/teams/TEAM1/members/user2@domain.com
-    Sample response:
-    HTTP 201
-    {
-      "data": {
-        "_id": "TEAM1",
-        "member_emails": ["user@domain.com", "user2@domain.com"],
-        "snake_url": "http://localhost:6000/"
-      },
-      "message": "Member added"
-    }
-    """
-    team = Team.find_one({'teamname': teamname})
-    if not team:
-        return _json_response(msg='Team not found', status=404)
-
-    if email not in team.member_emails:
-        team.member_emails.append(email)
-
-        team.save()
-
-        return _json_response(team.member_emails, msg='Member added', status=201)
-
-    return _json_response(team.member_emails, msg='Member already exists', status=200)

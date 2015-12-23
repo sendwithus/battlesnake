e349271eeb671f06cb491cbcb04428eafb20e257
Brad Van Vugt <bvanvugt@gmail.com>
2015-12-22 22:09:05 -0800

    Support Heroku Redis and MongoHQ.

--- settings/mongo.py
+++ settings/mongo.py
@@ -1,5 +1,17 @@
 from os import environ
 
-MONGODB_DATABASE = environ.get('MONGODB_DATABASE', 'battlesnake')
-MONGODB_URL = environ.get('MONGODB_URL', 'mongodb://localhost:27017/{}'.format(MONGODB_DATABASE))
+# Try MONGOHQ first...
+if environ.get('MONGOHQ_URL'):
+    MONGODB_URL = environ.get('MONGOHQ_URL')
+    MONGODB_DATABASE = MONGODB_URL.split('/')[-1]
+
+# Try local config
+else:
+    MONGODB_DATABASE = environ.get('MONGODB_DATABASE', 'battlesnake')
+    MONGODB_URL = environ.get(
+        'MONGODB_URL',
+        'mongodb://localhost:27017/{}'.format(MONGODB_DATABASE)
+    )
+
+# This probably will never be used.
 MONGODB_REPLICA_SET = environ.get('MONGODB_REPLICA_SET', None)
--- settings/redis.py
+++ settings/redis.py
@@ -1,5 +1,16 @@
 from os import environ
+from urlparse import urlparse
 
-REDIS_HOST = environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = int(environ.get('REDIS_PORT', '6379'))
-REDIS_DB = int(environ.get('REDIS_DB', '0'))
+
+# Try for Heroku Redis first
+if environ.get('REDIS_URL'):
+    parsed_url = urlparse(environ.get('REDIS_URL'))
+    REDIS_HOST = '%s:%s@%s' % (parsed_url.username, parsed_url.password, parsed_url.hostname)
+    REDIS_PORT = parsed_url.port
+    REDIS_DB = '0'
+
+# Try local config
+else:
+    REDIS_HOST = environ.get('REDIS_HOST', 'localhost')
+    REDIS_PORT = int(environ.get('REDIS_PORT', '6379'))
+    REDIS_DB = int(environ.get('REDIS_DB', '0'))

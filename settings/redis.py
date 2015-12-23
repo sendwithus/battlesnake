23b8c296066c0b14ad096ffa776a3ec76f00cc19
Brad Van Vugt <bvanvugt@gmail.com>
2015-12-22 22:23:50 -0800

    Use Heroku Redis password.

--- settings/redis.py
+++ settings/redis.py
@@ -5,12 +5,14 @@ from urlparse import urlparse
 # Try for Heroku Redis first
 if environ.get('REDIS_URL'):
     parsed_url = urlparse(environ.get('REDIS_URL'))
-    REDIS_HOST = '%s:%s@%s' % (parsed_url.username, parsed_url.password, parsed_url.hostname)
+    REDIS_HOST = parsed_url.hostname
     REDIS_PORT = parsed_url.port
+    REDIS_PASSWORD = parsed_url.password
     REDIS_DB = '0'
 
 # Try local config
 else:
     REDIS_HOST = environ.get('REDIS_HOST', 'localhost')
     REDIS_PORT = int(environ.get('REDIS_PORT', '6379'))
+    REDIS_PASSWORD = environ.get('REDIS_PASSWORD')
     REDIS_DB = int(environ.get('REDIS_DB', '0'))

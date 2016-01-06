9bfd35094761c36190826d02ad0890dd3c5eb883
Curtis Smith <curtis.smith8@gmail.com>
2016-01-05 18:20:09 -0800

    Return food in game serializer, made some simple localsnake ais

--- snakes/curtisss.py
+++ snakes/curtisss.py
@@ -6,7 +6,7 @@ class Snake(LocalSnake):
     def whois(self):
         return {
             'name': 'Curtisssnake',
-            'color': '#f0f087',
+            'color': '#002287',
             'head': 'wat'
         }
 
@@ -16,9 +16,38 @@ class Snake(LocalSnake):
         }
 
     def move(self, payload):
-        print payload
+        bad_tiles = []
+        for snake in payload['snakes']:
+            bad_tiles += snake['coords']
+            if snake['name'] == 'Curtisssnake':
+                head = snake['coords'][0]
+
+        smallest = 999
+        target = [0, 0]
+        for food in payload['food']:
+            x_dis = abs(food[0] - head[0])
+            y_dis = abs(food[1] - head[1])
+            distance = x_dis + y_dis
+            if distance < smallest:
+                smallest = distance
+                target = food
+
+        move = None
+        if target[1] < head[1]:
+            if [head[0], head[1]+1] not in bad_tiles:
+                move = 'north'
+        if target[1] > head[1]:
+            if [head[0], head[1]-1] not in bad_tiles:
+                move = 'south'
+        if target[0] < head[0]:
+            if [head[0]-1, head[1]] not in bad_tiles:
+                move = 'west'
+        if target[0] > head[0]:
+            if [head[0]+1, head[1]] not in bad_tiles:
+                move = 'east'
+
         return {
-            'move': 'north',
+            'move': move,
             'taunt': 'up up and away!'
         }
 

3ee1fedb85ad99bf81749cee805f30c04c5ec81c
Brandon Brown <brandon@brandonb.io>
2016-01-05 21:57:01 -0800

    Fix merge conflict

--- lib/server.py
+++ lib/server.py
@@ -27,26 +27,6 @@ def _form_error(msg, view=None):
     flash(msg, 'error')
     return redirect(url_for(view))
 
-@app.route('/')
-def index():
-    return app.send_static_file('src/html/index.2015.html')
-
-
-@app.route('/play/')
-@app.route('/play/<path:path>')
-@login_required
-def play(path=None):
-    # serve play.html for anything that starts with "play/"
-    # frontend will show the correct route
-    return app.send_static_file('src/html/play.html')
-
-
-@app.route('/static/<path:path>')
-def server_static(path):
-    # Flask has this built-in, but it's only active in dev
-    return send_from_directory('static', path)
-
-
 import lib.routes
 
 # Expose WSGI app

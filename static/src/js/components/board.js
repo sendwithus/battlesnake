618e96fb77756363b4d1bcb141925934f99ec289
Dimitri Tishchenko <dtishchenko@change.org>
2016-01-10 21:47:29 -0800

    render food as circles

--- static/src/js/components/board.js
+++ static/src/js/components/board.js
@@ -11,22 +11,18 @@ export default class Board {
 
   constructor (container) {
     this.container = container;
+
+    this.svg = d3.select(this.container)
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height);
   }
 
-  init (width, height) {
-    this.gameState = null;
+  init (width, height, gameState) {
+    this.gameState = gameState;
     this.dimensions = [ width, height ];
 
     this.resize.call(this);
-
-    if (!this.svg) {
-      this.svg = d3.select(this.container)
-        .append('svg')
-        .attr('width', this.width)
-        .attr('height', this.height);
-
-    }
-
     window.onresize = this.resize.bind(this);
   }
 
@@ -49,7 +45,6 @@ export default class Board {
       .attr('height', this.height);
 
     if (this.gameState) { this.update(this.gameState); }
-
   }
 
   update (gameState) {
@@ -58,7 +53,7 @@ export default class Board {
     var colours = {
         body: snakewithus.SQUARE_TYPES.SNAKE,
         empty: snakewithus.COLORS.EMPTY,
-        food: snakewithus.COLORS.FOOD,
+        food: snakewithus.COLORS.EMPTY,
         snake_head: snakewithus.SQUARE_TYPES.SNAKE_HEAD,
         gold: snakewithus.SQUARE_TYPES.GOLD
       },
@@ -67,6 +62,7 @@ export default class Board {
       spacing = snakewithus.SQUARE_PADDING,
       rows = this.svg.selectAll('g.row').data(boardData),
       cells = rows.selectAll('rect.cell').data(_.identity),
+      food = this.svg.selectAll('circle.food').data(gameState.food),
       xOffs = 0,
       yOffs = 0;
 
@@ -80,12 +76,15 @@ export default class Board {
     rows.enter()
       .append('g')
       .attr('class', 'row');
-    cells.enter()
-      .append('rect')
+    cells.enter().append('rect')
       .attr('class', 'cell')
       .attr('width', bitWidth - spacing)
       .attr('height', bitWidth - spacing)
       .attr('fill', function(d){ return colours[d.state] });
+    food.enter().append('circle')
+      .attr('class', 'food')
+      .attr('r', (this.SQUARE_SIZE / 2) - spacing)
+      .attr('fill', snakewithus.COLORS.FOOD);
 
     // transitions
     rows.transition().duration(0);
@@ -94,5 +93,17 @@ export default class Board {
       .attr('x', function(d, x, y){ return y * bitWidth + xOffs; })
       .attr('y', function(d, x, y){ return x * bitWidth + yOffs; })
       .attr('fill', function(d){ return colours[d.state] });
+    food.transition()
+      .each('start', function(d) {
+        d3.select(this).attr('r', 0);
+      })
+      .each('end', function(d) {
+        d3.select(this).attr('r', (bitWidth / 2) - spacing);
+      })
+      .duration(0)
+      .attr('cx', function(d){ return d[0] * bitWidth + xOffs + bitWidth / 2 - spacing / 2; })
+      .attr('cy', function(d){ return d[1] * bitWidth + yOffs + bitWidth / 2 - spacing / 2; });
+
+    food.exit().remove();
   }
 }

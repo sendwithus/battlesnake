c8828652793c5c09467c9f6fe4320660d0a07601
Brad Van Vugt <bvanvugt@gmail.com>
2016-02-02 17:50:54 -0800

    fix registration

--- lib/routes/admin.py
+++ lib/routes/admin.py
@@ -1,10 +1,15 @@
 from flask import (
     request, render_template, redirect, url_for, flash,
 )
+from pymongo.errors import DuplicateKeyError
 
+from lib.log import get_logger
 from lib.models.team import Team
 from lib.routes.auth import admin_only
-from lib.server import app
+from lib.server import app, form_error
+
+
+logger = get_logger(__name__)
 
 
 @app.route('/admin/teams')
@@ -14,6 +19,7 @@ def teams():
     teams.sort(key=lambda x: x.teamname)
 
     return render_template('admin/teams.html', teams=teams)
+
 
 @app.route('/admin/register', methods=['GET', 'POST'])
 @admin_only

e30a241390d305c9e0f0a1fd25952276166c6a08
Sean Lynch <seanlynch@gmail.com>
2016-02-04 16:33:46 -0800

    Remove unused Teams API endpoints. Can add them back later if necessary.

--- lib/routes/teams.py
+++ lib/routes/teams.py
@@ -1,21 +1,23 @@
 from flask import (
     request, g, session,
     render_template, redirect, url_for, flash,
-    abort,
+    abort
 )
-from flask.ext.login import login_user
-from pymongo.errors import DuplicateKeyError
 
-from lib.server import form_error, json_response, json_error, app
+from lib.server import form_error, json_response, app
 from lib.models.game import Game
 from lib.models.team import Team
-from lib.routes.auth import admin_only
 
 import lib.ai as ai
 import lib.game.engine as engine
 
 @app.route('/team', methods=['GET'])
 def get_team():
+    """
+    Show team profile page
+
+    Can be used by admins to view any team by adding ?admin_override_id=<team_id> to URL
+    """
     is_admin = (g.team.type == Team.TYPE_ADMIN)
     team = g.team
 
@@ -30,7 +32,12 @@ def get_team():
 
 
 @app.route('/team', methods=['POST'])
-def post_team():
+def update_team():
+    """
+    Update team profile
+
+    Can be used by admins to update any team by adding ?admin_override_id=<team_id> to URL
+    """
     is_admin = (g.team.type == Team.TYPE_ADMIN)
     team = g.team
     data = request.form
@@ -88,7 +95,7 @@ def team_test():
 
     # Fake a game
     game = Game(10, 10, 1)
-    snakes = [engine.Snake(g.team.snake_url)]
+    snakes = [engine.Snake(g.team.id, g.team.snake_url)]
     game_state = engine.Engine.create_game_state(game.id, game.width, game.height)
     engine.Engine.add_random_snakes_to_board(game_state, snakes)
 
@@ -130,134 +137,15 @@ def teams_list():
 
 @app.route('/api/teams/current')
 def team_info():
-    app.logger.info(session)
-    return json_response(data=g.team.serialize())
-
-
-# TODO remove this?
-@app.route('/api/teams/current', methods=['PUT'])
-def team_update():
-    team = g.team
-    data = request.get_json()
-    if not data:
-        return json_error(msg='Invalid team data', status=400)
-    if data.has_key('is_public') and not isinstance(data['is_public'], bool):
-        return json_error(msg='is_public must be a boolean or omitted', status=400)
-
-    # TODO: check for duplicate team name and allow updating
-    for field in ['snake_url', 'is_public']:
-        if field in data:
-            setattr(team, field, data[field])
-
-    # Handling checkboxes is weird
-    team.is_public = True if data.get('is_public') else False
-
-    team.save()
-    data = team.serialize()
-
-    # Log in team again in case team name changed
-    # Note: calling this seems to make team un-serializable
-    login_user(team)
-
-    return json_response(data=data)
-
-
-# TODO remove this?
-@app.route('/api/teams/current/members/<email>', methods=['PUT'])
-def team_member_create(email):
     """
-    Add a new member to an existing team.
-    Sample request:
-    PUT /api/teams/current/members/user2@domain.com
+    Get information about the current signed in team.
+
     Sample response:
-    HTTP 201
-    {
-      "data": {
-        "_id": "TEAM1",
-        "member_emails": ["user@domain.com", "user2@domain.com"],
-        "snake_url": "http://localhost:6000/"
-      },
-      "message": "Member added"
-    }
-    """
-
-    if email not in g.team.member_emails:
-        g.team.member_emails.append(email)
-
-        g.team.save()
-
-        return json_response(g.team.member_emails, msg='Member added', status=201)
-
-    return json_response(g.team.member_emails, msg='Member already exists', status=200)
-
-
-# TODO remove this?
-@app.route('/api/teams/<teamname>')
-@admin_only
-def team_details(teamname):
-    """
-    Get data for a single team.
-    Sample response:
-    {
-      "data": {
-        "_id": "TEAM1",
-        "member_emails": ["user@domain.com"],
-        "snake_url": "http://localhost:6000/"
-      }
-    }
-    """
-    team = Team.find_one({'teamname': teamname})
-    if not team:
-        return json_error(msg='Team not found', status=404)
-    return json_response(team.serialize())
-
-
-# TODO remove this?
-@app.route('/api/teams/', methods=['POST'])
-@admin_only
-def teams_create():
-    """
-    Create a new team and return it.
-    Sample request:
-    POST /api/teams/
     {
       "teamname": "TEAM1",
-      "snake_url": "http://localhost:6000/"
       "member_emails": ["user@domain.com"],
-    }
-    Sample response:
-    HTTP 201
-    {
-      "data": {
-        "teamname": "TEAM1",
-        "member_emails": ["user@domain.com"],
-        "snake_url": "http://localhost:6000/"
-      },
-      "message": "Team created"
+      "snake_url": "http://localhost:6000/"
     }
     """
-    data = request.get_json()
-    if not data:
-        return json_error(msg='Invalid team', status=400)
-
-    try:
-        teamname = data['teamname']
-        password = data['password']
-    except KeyError:
-        return json_error(msg='Invalid team, missing attributes', status=400)
-
-    snake_url = data.get('snake_url', None)
-    member_emails = data.get('member_emails', [])
-
-    existing_team = Team.find_one({'teamname': teamname})
-    if existing_team:
-        return json_error(msg='Team name already exists', status=400)
-
-    team = Team(teamname=teamname, password=password,
-                snake_url=snake_url, member_emails=member_emails)
-    try:
-        team.insert()
-    except DuplicateKeyError:
-        return json_error(msg='Team with that name already exists', status=400)
-
-    return json_response(team.serialize(), msg='Team created', status=201)
+    app.logger.info(session)
+    return json_response(data=g.team.serialize())
--- static/md/api.md
+++ static/md/api.md
@@ -4,7 +4,7 @@
 
 ### GET /api/teams/
 
-List all teams.
+List all public teams, as well as the currently signed in team (whether public or not)
 
 ##### Parameters
 None
@@ -47,93 +47,6 @@ None
       "teamname": "TEAM1",
       "member_emails": ["user@domain.com"],
       "snake_url": "http://localhost:6000/"
-    }
-  ]
-}
-```
-
-
-### PUT /api/teams/current
-
-Update the team that's currently signed in.
-
-##### Parameters
-* **snake_url** - Full URL to the team's current snake AI, or null if not set
-* **teamname** - Team name - *Not currently updateable*
-* **password** - The team's new password - *Not currently updateable*
-
-```json
-{
-  "teamname": "TEAM2",
-  "snake_url": "http://localhost:7000/",
-  "password": "SECRET"
-}
-```
-
-##### Response
-* **data** - The updated team object
-  * **teamname** - Team name
-  * **member_emails** - List of emails for members of this team
-  * **snake_url** - Full URL to the team's current snake AI, or null if not set
-
-```json
-{
-  "data": [
-    {
-      "teamname": "TEAM2",
-      "member_emails": ["user@domain.com"],
-      "snake_url": "http://localhost:7000/"
-    }
-  ]
-}
-```
-
-### PUT /api/teams/current/members/&lt;email&gt;
-
-Add a member to the team that's currently signed in.
-
-##### URL Parameters
-* **email** - The email of the user to add
-
-##### Response
-* **data** - The updated list of member emails on the current team
-
-```json
-{
-  "data": ["user@email.com", "user2@email.com"]
-}
-```
-
-### POST /api/teams/
-
-Create a new team.
-
-##### Parameters
-* **snake_url** - Full URL to the team's current snake AI, or null if not set
-* **teamname** - Team name - *Not currently updateable*
-* **password** - The team's new password - *Not currently updateable*
-
-```json
-{
-  "teamname": "TEAM2",
-  "password": "SECRET",
-  "snake_url": "http://localhost:8000/"
-}
-```
-
-##### Response
-* **data** - The new team object
-  * **teamname** - Team name
-  * **member_emails** - List of emails for members of this team
-  * **snake_url** - Full URL to the team's current snake AI, or null if not set
-
-```json
-{
-  "data": [
-    {
-      "teamname": "TEAM2",
-      "member_emails": [],
-      "snake_url": "http://localhost:7000/"
     }
   ]
 }
